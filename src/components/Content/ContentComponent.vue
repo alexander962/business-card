1a115bc65dd7046003a79c27ba5af6525454834d
Alexander962 <sanek12011996@gmai.com>
2022-06-05 16:31:48 +0300

    add About section

--- src/components/Content/ContentComponent.vue
+++ src/components/Content/ContentComponent.vue
@@ -1,22 +1,24 @@
 <template>
   <div class="content">
     <PromoComponent />
+    <AboutComponent />
   </div>
 </template>
 
 <script>
-
 import PromoComponent from "@/components/Content/Promo/PromoComponent";
+import AboutComponent from "@/components/Content/About/AboutComponent";
 
 export default {
-  name: 'ContentComponent',
+  name: "ContentComponent",
   props: {
-    msg: String
+    msg: String,
   },
   components: {
-    PromoComponent
-  }
-}
+    PromoComponent,
+    AboutComponent,
+  },
+};
 </script>
 
 <style lang="sass" scoped src="./Content.sass"></style>
